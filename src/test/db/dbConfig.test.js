5fd44e0935e8621379fbc75d052b6d5d382db24f
Vitor Soares <vitorsoaresf@gmail.com>
2024-07-30 07:45:39 -0300

    test: test connection db

---
+++ src/test/db/dbConfig.test.js
@@ -0,0 +1,20 @@
+import { describe } from "@jest/globals";
+import db from "../../db/dbconfig.js";
+
+describe("Test config db", () => {
+  it("should be check connection db", async () => {
+    const mockAuthor = {
+      nome: "Vitor",
+      nacionalidade: "Brasileiro",
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    };
+
+    const authorSave = await db("autores")
+      .insert(mockAuthor)
+      .then((res) => db("autores").where("id", res[0]))
+      .then((res) => res[0]);
+
+    expect(authorSave.nome).toBe(mockAuthor.nome);
+  });
+});
